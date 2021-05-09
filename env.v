0daabae4ebaff4bfb6ac3d42c4570f6e22e364cf
Ken <ken0x0a+github@gmail.com>
2021-05-10 06:06:32 +0900

    refactor: extract module

--- env.v
+++ env.v
@@ -10,7 +10,7 @@ import os
 // ```
 //
 // Example: load()
-pub fn load(path... string) map[string]string {
+pub fn load(path ...string) map[string]string {
 	// # this works only on $debug build
 	// mut file := $embed_file('.env')
 	// 
@@ -18,19 +18,19 @@ pub fn load(path... string) map[string]string {
 	// 	cstring_to_vstring(file.data())
 	// }
 
-	mut pa := ".env" // default value
+	mut pa := '.env' // default value
 	for p in path {
 		pa = p
 	}
-	str := os.read_file(pa) or {
-		panic("failed to read file $pa")
-	}
+	str := os.read_file(pa) or { panic('failed to read file $pa') }
 	mut conf := map[string]string{}
 	for line in str.split('\n') {
 		if line.len < 3 {
 			continue
 		}
-
+		if line.starts_with('#') {
+			continue
+		}
 		key_val := line.split_nth('=', 2)
 		conf[key_val[0]] = key_val[1]
 		// os.setenv(key_val[0], key_val[1], true)
