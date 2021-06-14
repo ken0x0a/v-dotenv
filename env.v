d456ce877265732ab1e2c46c9950c9cf98f2a201
Ken <ken0x0a+github@gmail.com>
2021-06-14 15:12:18 +0900

    feat(env): add fn `load`
    rename original fn to `parse`

--- env.v
+++ env.v
@@ -2,15 +2,18 @@ module env
 
 import os
 
-// load `.env` => map[string]string
+// load `.env`
 //
 // ```v
-// conf := load()  
-// conf["YOUR_ENV_VAR"]
+// import env  
+//
+// env.load()  
+// os.getenv("YOUR_ENV_VAR")
 // ```
 //
 // Example: load()
-pub fn load(path ...string) map[string]string {
+// Example: load("./path_to/.env")
+pub fn load(path ...string) {
 	// # this works only on $debug build
 	// mut file := $embed_file('.env')
 	// 
@@ -18,6 +21,33 @@ pub fn load(path ...string) map[string]string {
 	// 	cstring_to_vstring(file.data())
 	// }
 
+	mut pa := '.env' // default value
+	for p in path {
+		pa = p
+	}
+	str := os.read_file(pa) or { panic('failed to read file $pa') }
+	for line in str.split('\n') {
+		if line.len < 3 {
+			continue
+		}
+		if line.starts_with('#') {
+			continue
+		}
+		key_val := line.split_nth('=', 2)
+		os.setenv(key_val[0], key_val[1], true)
+	}
+}
+
+// parse `.env` => map[string]string
+//
+// ```v
+// conf := env.parse()  
+// conf["YOUR_ENV_VAR"]
+// ```
+//
+// Example: parse()
+// Example: parse("./path_to/.env")
+pub fn parse(path ...string) map[string]string {
 	mut pa := '.env' // default value
 	for p in path {
 		pa = p
